538cb9badba2a8e7ace7e6a8c925aeb37914cdde
TINGWEIJING <tingweijingting2000@gmail.com>
2020-11-24 00:33:03 +0800

    Add UI elements

--- src/main/java/autoClick/App.java
+++ src/main/java/autoClick/App.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -17,14 +18,29 @@ import javafx.stage.WindowEvent;
 public class App extends Application {
 
     public static final Pane mainPane = new Pane();
-    public static final Label MOUSE_X_LB = new Label("Position X:");
-    public static final Label MOUSE_Y_LB = new Label("Position Y:");
+    public static final Label MOUSE_X_LB = new Label("X:");
+    public static final Label MOUSE_Y_LB = new Label("Y:");
+    public static final Label EMAIL_LB = new Label("Email format:");
+    public static final Label TOTAL_COUNT_LB = new Label("Total count:");
+    public static final Label COUNTER_LB = new Label("Counter:");
+    public static final Label START_RECORD_LB = new Label("Start Record: Ctrl + F1");
+    public static final Label STOP_RECORD_LB = new Label("Stop Record: Ctrl + F2");
+    public static final Label START_OPERATION_LB = new Label("Start Operation: Ctrl + F12");
+    public static final Label STOP_OPERATION_LB = new Label("Stop Operation: Pause Break");
+
+    public static final Button BROWSE_BT = new Button("Browse");
+    public static final Button RECORD_BT = new Button("Record");
     public static final Button GO_BT = new Button("GO");
 
-    public static final Font DEFAULT_FONT = Font.font("Arial", 18);
+    public static final Font DEFAULT_FONT = Font.font("Segoe UI", 14);
 
     public static Label mousePosX = new Label("0");
     public static Label mousePosY = new Label("0");
+    public static Label emailExample = new Label("thomas@");
+    public static Label totalCount = new Label("0");
+    public static Label counter = new Label("0");
+    public static TextField srcDir = new TextField("C:/");
+    public static TextField emailSuffix = new TextField("siswa.um.edu.my");
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -37,8 +53,8 @@ public class App extends Application {
 
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setResizable(false);
-        stage.setHeight(240);
-        stage.setWidth(280);
+        stage.setHeight(480);
+        stage.setWidth(400);
         stage.setTitle("Auto Click");
         stage.setScene(scene);
         stage.show();
@@ -58,31 +74,63 @@ public class App extends Application {
     }
 
     private void customizePane() {
-        MOUSE_X_LB.setPrefSize(100, 20);
         MOUSE_X_LB.setFont(DEFAULT_FONT);
-        setEleSize(MOUSE_X_LB, 20, 20);
+        customizeElement(MOUSE_X_LB, 20, 20, 20, 10);
 
-        MOUSE_Y_LB.setPrefSize(100, 20);
         MOUSE_Y_LB.setFont(DEFAULT_FONT);
-        setEleSize(MOUSE_Y_LB, 20, 50);
+        customizeElement(MOUSE_Y_LB, 20, 20, 100, 10);
 
-        mousePosX.setPrefSize(80, 20);
-        mousePosX.setFont(DEFAULT_FONT);
-        setEleSize(mousePosX, 120, 20);
-
-        mousePosY.setPrefSize(80, 20);
-        mousePosY.setFont(DEFAULT_FONT);
-        setEleSize(mousePosY, 120, 50);
-
-        GO_BT.setPrefSize(120, 60);
+        BROWSE_BT.setFont(DEFAULT_FONT);
+        customizeElement(BROWSE_BT, 80, 20, 300, 40);
+        
+        EMAIL_LB.setFont(DEFAULT_FONT);
+        customizeElement(EMAIL_LB, 100, 20, 20, 70);
+        
+        TOTAL_COUNT_LB.setFont(DEFAULT_FONT);
+        customizeElement(TOTAL_COUNT_LB, 100, 20, 20, 100);
+        
+        COUNTER_LB.setFont(DEFAULT_FONT);
+        customizeElement(COUNTER_LB, 100, 20, 20, 130);
+        
+        RECORD_BT.setFont(DEFAULT_FONT);
+        customizeElement(RECORD_BT, 180, 20, 20, 390);
+        
         GO_BT.setFont(DEFAULT_FONT);
-        setEleSize(GO_BT, 80, 160);
+        customizeElement(GO_BT, 180, 20, 200, 390);
+        
+        START_RECORD_LB.setFont(DEFAULT_FONT);
+        customizeElement(START_RECORD_LB, 180, 20, 20, 420);
+        
+        STOP_RECORD_LB.setFont(DEFAULT_FONT);
+        customizeElement(STOP_RECORD_LB, 180, 20, 20, 450);
+        
+        START_OPERATION_LB.setFont(DEFAULT_FONT);
+        customizeElement(START_OPERATION_LB, 180, 20, 200, 420);
+        
+        STOP_OPERATION_LB.setFont(DEFAULT_FONT);
+        customizeElement(STOP_OPERATION_LB, 180, 20, 200, 450);
+        
+        mousePosX.setFont(DEFAULT_FONT);
+        customizeElement(mousePosX, 40, 20, 40, 10);
+
+        mousePosY.setFont(DEFAULT_FONT);
+        customizeElement(mousePosY, 120, 20, 120, 10);
+
 
         mainPane.getChildren().addAll(MOUSE_X_LB,
                                       MOUSE_Y_LB,
+                                      EMAIL_LB,
+                                      COUNTER_LB,
+                                      TOTAL_COUNT_LB,
+                                      START_RECORD_LB,
+                                      STOP_RECORD_LB,
+                                      START_OPERATION_LB,
+                                      STOP_OPERATION_LB,
+                                      BROWSE_BT,
+                                      RECORD_BT,
+                                      GO_BT,
                                       mousePosX,
-                                      mousePosY,
-                                      GO_BT);
+                                      mousePosY);
 
     }
 
@@ -96,7 +144,8 @@ public class App extends Application {
 
     }
 
-    public void setEleSize(Region element, int x, int y) {
+    public void customizeElement(Region element, int width, int height, int x, int y) {
+        element.setPrefSize(width, height);
         element.setLayoutX(x);
         element.setLayoutY(y);
     }
